cde970339e00a73fae4ddd5b1ac5dbbd1b205d74
ehsan <e@ewerx.ca>
2022-01-22 19:15:25 -0500

    comment out on-chain graph, add events, add basic tests

--- contracts/ProfileGraph.sol
+++ contracts/ProfileGraph.sol
@@ -5,18 +5,33 @@ pragma solidity ^0.8.0;
 import "./ProfileToken.sol";
 
 contract ProfileGraph is ProfileToken {
-    mapping(uint256 => uint256[]) public followers;
-    mapping(uint256 => uint256[]) public following;
 
-    event NewFollower(uint256 follower, uint256 following);
-    event RemovedFollower(uint256 follower, uint256 following);
+    // data
 
-    function follow(uint256 _followed, uint256 _follower) public tokenOwner(_follower) {
+    //mapping(uint256 => uint256[]) public _followers;
+    //mapping(uint256 => uint256[]) public _following;
+
+    // events
+
+    event NewFollow(uint256 follower, uint256 following);
+    event UnFollow(uint256 follower, uint256 following);
+
+    // functions
+
+    function follow(uint256 toFollow, uint256 byFollower) public tokenOwner(byFollower) {
         //TODO: check existing?
-        following[_follower].push(_followed);
-        followers[_followed].push(_follower);
-        emit NewFollower(_follower, _followed);
+        //_following[byFollower].push(toFollow);
+        //_followers[toFollow].push(byFollower);
+        emit NewFollow(byFollower, toFollow);
     }
+
+    function unfollow(uint256 toUnfollow, uint256 byFollower) public tokenOwner(byFollower) {
+        //_following[byFollower].remove(toUnfollow);
+        //_followers[toUnfollow].remove(byFollower);
+        emit UnFollow(toUnfollow, byFollower);
+    }
+
+    // modifiers
 
     modifier tokenOwner(uint256 _tokenId) {
         require(msg.sender == ERC721.ownerOf(_tokenId));
