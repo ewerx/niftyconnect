23cb90031b0d5bd33ef8e229992b12d834ec147d
6rivu <68269450+6rivu@users.noreply.github.com>
2022-01-24 00:39:41 +0530

    Frontend files layout, adding register profile page (#4)
    * Frontend files layout, adding register profile page
    
    * Adding create-profile frontend code.
    
    Made the changes prescribed by @gabriel.

---
+++ src/pages/register.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import './register.css';
+
+const Register = (props) => {
+
+  //State variables
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [description, setDescription] = useState("");
+  const [name, setName] = useState("");
+  const [url, setURL] = useState("");
+
+  const connectWallet = async () => { //TODO: connect to wallet
+   
+  };
+
+  const mint = async () => { //TODO: mint profile token
+    
+  };
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWallet}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          walletAddress.substring(0, 6) +
+          "..." +
+          walletAddress.substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+
+      <br></br>
+      <h1 id="title">🧙‍♂️ Profile Minter</h1>
+     
+      <form>
+        <h2>🌈 Avatar </h2>
+        <input
+          id="avatar"
+          type="text"
+          placeholder="e.g. https://opensea.io/assets/your-asset"
+          onChange={(event) => setURL(event.target.value)}
+        />
+        <h2>⚡ENS Address: </h2>
+        <input
+          id="ens"
+          type="text"
+          placeholder="e.g. name.eth"
+          onChange={(event) => setName(event.target.value)}
+        />
+        
+        <h2>✍️ Tagline: </h2>
+        <input
+          id='tagline'
+          type="text"
+          placeholder="e.g. To the moon🚀 ;)"
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={mint}>
+        Mint Profile
+      </button>
+      <p id="status">
+        {status}
+      </p>
+    </div>
+  );
+};
+
+export default Register;
