9abf30b7e937c30865117b9813edeca2b381c601
Gabriel Song <gabsong@users.noreply.github.com>
2022-01-27 19:53:10 -0800

    Enable wallet connection, but mint NFT fails due to cannot estimate gas error (#10)
    * Enable wallet connection, but mint NFT fails due to cannot estimate gas error
    
    * Revert contract address change
    
    * Lint
    
    * Lint
    
    * Add codeowners
    
    Co-authored-by: Gabriel Song <gabrielsong@Gabriels-MacBook-Pro-14.local>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home.js";
-import MintProfile from "./pages/mintprofile.js";
-import ProfileHome from "./pages/profilehome.js";
+import Dapp from "./components/Dapp";
 
 export default function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="mintprofile" element={<MintProfile />} />
-        <Route path="profilehome" element={<ProfileHome />} />
-      </Routes>
-    </>
+    <Dapp />
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.js";
-
-// Add this in node_modules/react-dom/index.js
-window.React1 = require('react');
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,5 +9,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.querySelector("#root")
+  document.getElementById("root")
 );
