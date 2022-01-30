b5866dce9757d97c1bbb239df60ef4891cf07e60
Gabriel Song <gabrielsong@Gabriels-MacBook-Pro-14.local>
2022-01-30 04:50:09 -0800

    Implement followers + unfollow

--- src/components/socialgraph.js
+++ src/components/socialgraph.js
@@ -1,41 +1,90 @@
-// social graph component (on left portion of profilehome, below header)
-
 import React, { useState, useEffect } from 'react';
-import { Grid } from '@mui/material';
-import { requestAccount } from '../utils/common';
-// import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import contractAddress from '../abis/contract-address.json';
+import contractArtifact from '../abis/ProfileGraphContract.json';
+import { useQuery } from '@apollo/client';
+import { Button, Grid } from '@mui/material';
+import { followers, following }  from '../utils/queries';
+import { follow, unfollow } from '../utils';
 
-export default function Socialgraph ({ selectedProfileId }) {
-  const [account, setAccount] = useState('');
+export default function SocialGraph ({ tokenId }) {
+  const [unfollowRequester, setUnfollowRequester] = useState('');
+  const [, setUnfollowLoading] = useState(false);
+  const [unfollowed, setUnfollowed] = useState('');
+  const CONTRACT_ADDR = contractAddress.ProfileGraph;
 
-  useEffect(() => {
-    async function doSomething () {
-      const accounts = await requestAccount();
-      setAccount(accounts[0]);
+  const { loading, error, data } = useQuery(followers,
+    { variables: { tokenId } });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Select Profile Token!</p>;
+  if (!data.followers.length) return <p>No followers found</p>;
+
+  const followerList = data.followers.map(({ profile }) => profile);
+
+  async function handleUnfollow (unfollowed, unfollowRequester) {
+    setUnfollowLoading(true);
+    try {
+      await unfollow(CONTRACT_ADDR, contractArtifact, unfollowed, unfollowRequester);
+    } catch (err) {
+      console.error('handleUnfollow error');
     }
-    doSomething();
-  }, []);
-
+    setUnfollowLoading(false);
+    setUnfollowRequester('');
+    setUnfollowed('');
+  }
+  
   return (
     <Grid container direction='row' justifyContent='center' spacing='50'>
       <Grid item md={12} textAlign='center'>
         <h1>Social Graph</h1>
       </Grid>
       <Grid item md={6} textAlign='center' sx={{ height: '40%', pt: 0 }}>
-        {/* vv PFP token image should be mapped here */}
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHIolqCfnkvob1BEoALzK3wZdwYAEmAvUuOtAi8IwSshYzogYSz6QO_TBPsyGScmKlMgg&usqp=CAU' alt='' />
-        <h1>ape102.eth</h1>
+        <h1>Followers</h1>
+        {followerList.map((profile, index) => (
+          <Follower 
+            profile={profile}
+            myTokenId={tokenId}
+            key={`${profile.tokenID}-${index}`}
+            handleUnfollow={handleUnfollow}
+          />
+        ))}
       </Grid>
       <Grid item md={6} textAlign='center' sx={{ height: '40%', pt: 0 }}>
-        <h3>Address</h3>
-        <p>{account}</p>
-      </Grid>
-      <Grid item md={6} textAlign='center'>
-        <h1>Followers</h1>  {/* List followers and follower count of connected user */}
-      </Grid>
-      <Grid item md={6} textAlign='center'>
-        <h1>Following</h1>  {/* List followings and following count of connected user */}
+        {/** TODO */}
       </Grid>
     </Grid>
   );
+}
+
+const Follower = function({ profile, myTokenId, handleUnfollow }) {
+  const { tokenID, contentURI } = profile;
+  const [tokenURI, setTokenURI] = useState('');
+  
+  useEffect(() => {
+    fetchTokenURI(contentURI).then(tokenURI => {
+      setTokenURI(tokenURI);
+    });
+  }, []);
+
+  return (
+    <>
+      <p>{`NFTC#${tokenID}`}</p>
+      {
+        tokenURI && (
+          <img src={tokenURI} />
+        )
+      }
+      <Button variant='text' onClick={() => handleUnfollow(tokenID, myTokenId)}>Unfollow</Button>
+    </>
+  );
+}
+
+const fetchTokenURI = async function (contentURI) {
+  let tokenURI = '';
+  if (contentURI) {
+    const res = await fetch(contentURI);
+    const json = await res.json();
+    tokenURI = json.image;
+  }
+  return tokenURI;
 }
--- src/pages/profilehome.js
+++ src/pages/profilehome.js
@@ -1,19 +1,9 @@
-import { Grid, Container } from '@mui/material';
-import UserTokens from '../components/UserTokens';
+import { UserTokens } from '../components/UserTokens';
 
 export default function ProfileHome () {
   return (
     <>
       <UserTokens accountId='0x7c281214ea57273634b6f22a83958495be66b38e' />
-      {/* <Container maxWidth='xl'>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-
-          </Grid>
-          <Grid item xs={4} />
-          <Grid item xs={4} />
-        </Grid>
-      </Container> */}
     </>
   );
 }
