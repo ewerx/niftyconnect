c92da12517633b2f18dd4bbe7aa8805c4eecbd49
Gabriel Song <gabrielsong@Gabriels-MacBook-Pro-14.local>
2022-01-29 23:12:05 -0800

    Define queries

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,21 +1,29 @@
 import { gql } from '@apollo/client';
 
-// TODO: get profiles that match an owner.id
-const profiles = gql`
-  query getProfiles {
-    profiles {
+/**
+ * Get all profiles that belong to the user.
+ * Pass the property `variables: { id: accountIdOfUser }` with the query.
+ * i.e. 0x7c281214ea57273634b6f22a83958495be66b38e
+ */
+const userProfiles = gql`
+  query getUserProfiles {
+    user: users(where: {id: $accountId}) {
       id
-      tokenID
-      owner {
-        id
+      profiles {
+        tokenID
+        contentURI
       }
     }
   }
 `;
 
+/**
+ * Filter a list of profiles by tokenId, to get "profiles followed by me".
+ * Pass the property `variables: { tokenId: tokenIdOfUser }` with the query.
+ */
 const following = gql`
-  query getFollowingList {
-    followingList: followers(where:{tokenID: $tokenId}) {
+  query getFollowing {
+    following: followers(where:{tokenID: $tokenId}) {
       ...fields
     }
   }
@@ -25,7 +33,22 @@ const following = gql`
     Token: following {
       tokenID
       contentURI
-      metadataURI
+    }
+  }
+`;
+
+/**
+ * Get list of followers, "profiles that follow me"
+ * Pass the property `variables: { tokenId: tokenIdOfUser }` with the query.
+ */
+const followers = gql`
+  query getFollowers {
+    followers(where:{tokenID: $tokenId}) {
+      tokenID
+      profile: following {
+        tokenID
+        contentURI
+      }
     }
   }
 `;
